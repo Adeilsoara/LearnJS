5e7ad43752e2202a523b8f1a7bb0701968e3f780
Adeilsoara <adeilsonaragao@hotmail.com>
2022-02-24 08:19:06 -0300

    IMCCalculadora - Modificação na coleta de dados dos campos de input

--- IMCCalculadora/script.js
+++ IMCCalculadora/script.js
@@ -2,22 +2,9 @@ let meuForm = document.getElementById("meuForm")
 let meuNome = document.getElementById("nome")
 let altura = document.getElementById("altura")
 let peso = document.getElementById("peso")
-let thNome = document.getElementById("thNome")
-let thAltura = document.getElementById("thAltura")
 
 meuForm.addEventListener("submit", (e) =>{
-    e.preventDefault()
-    createItem(meuNome.value)
     
 })
 
-function createItem(x, y) {
-    let ourHTML = `<th>${x}</th> <th>${y}</th>`
-    thNome.insertAdjacentHTML("beforeend", ourHTML)
-    thAltura.insertAdjacentHTML("beforeend", ourHTML)
-    altura.value = ""
-    meuNome.value = ""
-    meuNome.focus()
-    altura.focus()
-    
-}
+
