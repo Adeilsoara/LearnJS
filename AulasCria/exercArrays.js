2f26d94e3949a1e70fe4f562b8ede6cb887ee102
Adeilsoara <adeilsonaragao@hotmail.com>
2022-03-03 16:08:25 -0300

    Exercícios modificação

--- AulasCria/exercArrays.js
+++ AulasCria/exercArrays.js
@@ -2,24 +2,24 @@
 let numeros = [5, 4, 3, 2, 1]
 
 /* função push */
-numeros.push(0)
+/* numeros.push(0)
 
-document.write(numeros + '<br>')
+document.write(numeros + '<br>') */
 
 /* inverter a ordem do array */
 
-document.write(numeros.reverse() + '<br>')
+/* document.write(numeros.reverse() + '<br>') */
 
 /* Método Map */
 
-numeroComMap = numeros.map(numero => numero * numero)
+/* numeroComMap = numeros.map(numero => numero * numero)
 
-document.write(`${numeroComMap} <br>`)
+document.write(`${numeroComMap} <br>`) */
 
 /* Método filter */
 
-numeroComFilter = numeros.filter(numero => numero >= 3)
-document.write(`Números filtrados : ${numeroComFilter}`)
+/* numeroComFilter = numeros.filter(numero => numero >= 3)
+document.write(`Números filtrados : ${numeroComFilter}`) */
 
 /* Preencher um vetor com os valores informados pelo usuário */
 /* arrayLetras = []
@@ -32,6 +32,6 @@ document.write(`${arrayLetras}`) */
 /* Converter o array de letras para todas as letras maiusculas */
 
 let letras= ['m', 'a' , 'n', 'o', 'e', 'l']
+let maiusculas = letras.map(p => p.toUpperCase());
+document.write(`Letras MAIÚSCULAS: ${maiusculas}`)
 
-let letrasMaiusculas = letras.toUpperCase()
-document.write(`Letras MAIÚSCULAS: ${letrasMaiusculas}`)
