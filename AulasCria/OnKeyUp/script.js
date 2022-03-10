8a46c30e84d08ebefbb8c69c2c677baca667dfee
Adeilsoara <adeilsonaragao@hotmail.com>
2022-03-10 13:18:49 -0300

    Comentários nas funções

--- AulasCria/OnKeyUp/script.js
+++ AulasCria/OnKeyUp/script.js
@@ -12,23 +12,26 @@ for (let i = 0; i < cargos.length; i++) {
     ul.appendChild(li) 
 }
 
+//selecionar o que está sendo digitado no campo input
 let inputTexto = document.querySelector(".inputTexto")
 
 function filtrarCargos() {
     console.log(inputTexto.value)
 
+    //array para os cargos que serão filtratos
     let cargosFiltrados = []
     if (inputTexto.value != "") {
-        //limpar a tela
+
+        //limpar a tela enquanto o usuário digita no input
         ul.innerText = ""
 
-        // filtrar os nomes com a letra digitada no input
+        // filtrar os nomes com a letra digitada no input e jogar dentro do array cargosFiltrados
         for (let i = 0; i < cargos.length; i++) {
             if (cargos[i].includes(inputTexto.value)) {
                 cargosFiltrados.push(cargos[i])
             } 
         }
-
+        // para cada elemento que foi sendo encontrado no array cargosFiltrados vai sendo criada a sua lista
         for (let i = 0; i < cargosFiltrados.length; i++) {
             const li = document.createElement('li')
             li.innerText = cargosFiltrados[i]
@@ -45,3 +48,7 @@ function filtrarCargos() {
         }
     }
 }
+
+/* window.addEventListener('load', (e) => {
+    alert('página carregada')
+}) */
