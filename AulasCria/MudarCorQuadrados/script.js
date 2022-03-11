7f4ed4820865e5eb987564222c5194e04232831c
Adeilsoara <adeilsonaragao@hotmail.com>
2022-03-11 10:35:22 -0300

    Utilizando arrow functions

--- AulasCria/MudarCorQuadrados/script.js
+++ AulasCria/MudarCorQuadrados/script.js
@@ -2,36 +2,31 @@ let mudarCor1 = document.getElementById("q1")
 let mudarCor2 = document.getElementById("q2")
 let mudarCor3 = document.getElementById("q3")
 
-mudarCor1.addEventListener("mouseover", mudarCor) 
-mudarCor1.addEventListener("mouseout", voltarCor)
+mudarCor1.addEventListener("mouseover", () =>{
+    mudarCor1.style.background = "green"
+})
 
-mudarCor2.addEventListener("mouseover", mudarCorQuadrado2)
-mudarCor2.addEventListener("mouseout", voltarCorQuadrado2)
-
-mudarCor3.addEventListener("mouseover", mudarCorQuadrado3)
-mudarCor3.addEventListener("mouseout", voltarCorQuadrado3)
-
-function mudarCor() {
-     mudarCor1.style.background = "green"
-    //mudarCor1.style.background = "url('https://source.unsplash.com/random/200x200')";  
-
-}
-function voltarCor() {
+mudarCor1.addEventListener("mouseout", ()=>{
     mudarCor1.style.background = ""
-}
+})
 
-function mudarCorQuadrado2() {
+mudarCor2.addEventListener("mouseover", ()=>{
     mudarCor2.style.background = "red"
-}
+})
 
-function voltarCorQuadrado2() {
+mudarCor2.addEventListener("mouseout", ()=>{
     mudarCor2.style.background = ""
-}
+})
 
-function mudarCorQuadrado3() {
+mudarCor3.addEventListener("mouseover", ()=>{
     mudarCor3.style.background = "blue"
-}
+})
 
-function voltarCorQuadrado3() {
+mudarCor3.addEventListener("mouseout", ()=>{
     mudarCor3.style.background = ""
-}
+})
+
+
+
+
+
