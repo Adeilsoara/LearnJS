118f9277365807bd2960236d53d03a727798c83b
Adeilsoara <adeilsonaragao@hotmail.com>
2022-03-13 12:18:49 -0300

    Aulas sobre o loop For

---
+++ AulasCria/AulasFor/script.js
@@ -0,0 +1,10 @@
+//Somar elementos
+let soma = 0
+const n = 10
+
+for (let i = 0; i < n; i++) {
+    soma += i // soma = soma + i
+    document.write(`A soma do ${i}ª = ${soma} <br>`)
+}
+
+document.write(`A soma total é: ${soma}`)
