232eeb4b6959dd09b5a53a4c838d03d3d2fbf277
Adeilsoara <adeilsonaragao@hotmail.com>
2022-03-17 22:23:44 -0300

    teste

--- Teste/script.js
+++ Teste/script.js
@@ -2,23 +2,25 @@
 const numero = Math.floor(Math.random() * numeros.length)
 console.log(numeros[numero]) */
 
-const arr = ['a', 'c', 'd', 'b'];
+const arr = ['a', 'c', 'd', 'b']
+
 
 /* for (let i = 0; i < 30; i++) {
   arr.push(i)
 }  */      
 
 //Código para ordenar o Array de forma aletória
-/* for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
  const j = Math.floor(Math.random() * (i + 1));
- [arr[i], arr[j]] = [arr[j], arr[i]];
+ let sorteio = [arr[i], arr[j]] = [arr[j], arr[i]];
+ console.log(sorteio)
 }
 
 console.log(arr)
 
-document.write(arr) */
+document.write(arr)
 
-function listarTabela(){
+/* function listarTabela(){
     let tbody = document.getElementById('tbody')
     for (let i = 0; i < arr.length; i++) {
         let tr = tbody.insertRow()
@@ -27,10 +29,7 @@ function listarTabela(){
         let tdValor2 = tr.insertCell()
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
-        tdValor1.innerText = arr[i]
-        tdValor2.innerText = arr[i]
-
-        
+        tdValor1.innerText = arr[i]        
     }
 }
-listarTabela()
+listarTabela() */
