f84f8f091a4758ac9e13041c2358798823be88c7
Adeilsoara <adeilsonaragao@hotmail.com>
2022-03-19 18:12:49 -0300

    Comentários no código

--- AulasCria/CalculadoraDeIdade/script.js
+++ AulasCria/CalculadoraDeIdade/script.js
@@ -1,13 +1,17 @@
 function calcularIdade() {
+    //Nesta 1ª parte capturamos o que o usuário digita nos inputs
     let dia1 = document.getElementById('dia').value
     let mes1 = document.getElementById('mes').value
     let ano1 = document.getElementById('ano').value
-
+    
+    //Para fazer o cálculo da idade é necessário pegar a data do dia atual
+    //Nesta parte do código fazemos isso
     let data = new Date()
     let dia2 = data.getDate()
-    let mes2 = 1 + data.getMonth()
+    let mes2 = 1 + data.getMonth() //Como os meses em javascript iniciam de 0 aqui acrestamos 1 para que a contagem vá de 1 até 12, ao invés de 0 até 11
     let ano2 = data.getFullYear()
 
+    //Criamos um array com a quantidade de dias de cada mês do ano
     let ultimoDiaDeCadaMes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     if (dia1 > dia2) {
