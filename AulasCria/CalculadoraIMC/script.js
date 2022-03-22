382f4556460c372dfb6148e2294a15a94d8511b9
Adeilsoara <adeilsonaragao@hotmail.com>
2022-03-22 16:24:30 -0300

    Calculadora IMC - Faixas de Peso

--- AulasCria/CalculadoraIMC/script.js
+++ AulasCria/CalculadoraIMC/script.js
@@ -4,6 +4,20 @@ function imc() {
 
     let calculoIMC = (peso/ (Math.pow(altura,2))).toFixed(2)
     
-    document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC}`
+    let faixasDePeso = ['Magreza', 'Saudável', 'Sobrepeso', 'Obesidade Grau I']
+    if (calculoIMC < 18.5) {
+        document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Magreza`
+    } else if(calculoIMC >= 18.5 && calculoIMC < 24.9){
+        document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Saudável`
+    } else if(calculoIMC >= 25 && calculoIMC < 29.9){
+        document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Sobrepeso`
+    } else if(calculoIMC >= 30 && calculoIMC < 34.9){
+        document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Obesidade Grau I`
+    } else if(calculoIMC >= 35 && calculoIMC < 39.9){
+        document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Obesidade Grau II`
+    }else{
+        document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Obesidade Mórbida`
+    }
+    
 }
 
