15cd2fa765cb5875b7fedc7ef3c1d0092c21a339
Adeilsoara <adeilsonaragao@hotmail.com>
2022-03-23 17:19:46 -0300

    Calculadora IMC - Adcionando elementos na tabela

--- AulasCria/CalculadoraIMC/script.js
+++ AulasCria/CalculadoraIMC/script.js
@@ -4,11 +4,11 @@ function imc() {
 
     let calculoIMC = (peso/ (Math.pow(altura,2))).toFixed(2)
     
-    let faixasDePeso = ['Magreza', 'Saudável', 'Sobrepeso', 'Obesidade Grau I']
+    //let faixasDePeso = ['Magreza', 'Saudável', 'Sobrepeso', 'Obesidade Grau I']
     if (calculoIMC < 18.5) {
-        document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Magreza`
+        document.getElementById('thFaixa').innerHTML = 'Magreza'
     } else if(calculoIMC >= 18.5 && calculoIMC < 24.9){
-        document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Saudável`
+        document.getElementById('thFaixa').innerHTML = 'Saudável'
     } else if(calculoIMC >= 25 && calculoIMC < 29.9){
         document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Sobrepeso`
     } else if(calculoIMC >= 30 && calculoIMC < 34.9){
@@ -18,6 +18,11 @@ function imc() {
     }else{
         document.getElementById('resultado').innerText = `O seu IMC é de: ${calculoIMC} e sua faixa é: Obesidade Mórbida`
     }
+
+    document.getElementById('thPeso').innerHTML = peso
+    document.getElementById('thAltura').innerHTML = altura
+    document.getElementById('thImc').innerHTML = calculoIMC
     
-}
+  }
+          
 
