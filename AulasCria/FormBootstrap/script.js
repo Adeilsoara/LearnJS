7901a690334292cf24f7cdddf0ac8b2009ff5f82
Adeilsoara <adeilsonaragao@hotmail.com>
2022-04-06 08:05:09 -0300

    Mascara para CEP

--- AulasCria/FormBootstrap/script.js
+++ AulasCria/FormBootstrap/script.js
@@ -19,3 +19,16 @@ campoDataDeNascimento.addEventListener('change', (e) => {
 
     campoIdade.value = capturarIdade(data)
 })
+
+function mascaraCep(mascaraInput) {
+    const tamanhoInput = document.getElementById('cepInput').maxLength
+    let valorInput = document.getElementById('cepInput').value
+    //console.log('tamanho máximo:', tamanhoInput, 'valor input', valorInput)
+    const mascara = {
+        cep : valorInput.replace(/[^\d]/g, "").replace(/^(\d{5})(\d{3}).*/, '$1-$2' )
+    };
+
+    if(valorInput.length == tamanhoInput){
+        document.getElementById('cepInput').value = mascara[mascaraInput]
+    }
+}
