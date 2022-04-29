81da16088bd225bbbaf1688e9f119442e5fe2cc7
Adeilsoara <adeilsonaragao@hotmail.com>
2022-04-29 16:32:08 -0300

    Modficação no Script

--- GeradorDeSenhas2b/script.js
+++ GeradorDeSenhas2b/script.js
@@ -6,31 +6,14 @@ function gerarSenha() {
     Math.random().toString(36).toUpperCase().slice(2)
 }
 
-/* document.getElementById('copiar').addEventListener('click', function () {
-    var texto = document.getElementById('resultado').innerText;
-  
-    function listener(e) {
-      e.clipboardData.setData('text/plain', texto);
-      e.preventDefault();
-    }
-  
-    document.addEventListener('copy', listener);
-    document.execCommand('copy');
-    document.removeEventListener('copy', listener);
-  });
- */
+ copiaEl.addEventListener('click', () => {
+   const textArea = document.createElement('textarea')
+   const senha = resultadoEl.innerText
 
-  /* Função para copiar os caracteres gerados no campo resultado */
-  copiaEl.addEventListener('click', () => {
-    const textArea = document.createElement('textarea')
-    const senha = resultadoEl.innerText
-
-    /* if(!senha) { return } */
-
-    textArea.value = senha
-    document.body.appendChild(textArea)
-    textArea.select()
-    document.execCommand('copy')
-    textArea.remove()
-    alert('Senha copiada!')
-})
+   textArea.value = senha 
+   document.body.appendChild(textArea)
+   textArea.select()
+   document.execCommand('copy')
+   textArea.remove()
+   alert('Senha copiada!')
+ })
