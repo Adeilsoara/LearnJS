5823dbe5c8162b3039e1c7290d4293395c65aab4
Adeilsoara <adeilsonaragao@hotmail.com>
2022-05-02 22:03:58 -0300

    Armazenamento local

--- AulasCria/GeradorDeSenhas/script.js
+++ AulasCria/GeradorDeSenhas/script.js
@@ -6,6 +6,7 @@ const copiaEl = document.getElementById('copiar')
 function gerarSenha() {
     resultadoEl.innerText = Math.random().toString(36).slice(2) +
     Math.random().toString(36).toUpperCase().slice(2)
+    
 }
 
 resultadoEl.addEventListener('click', gerarSenha)
@@ -15,11 +16,26 @@ resultadoEl.addEventListener('click', gerarSenha)
 copiaEl.addEventListener('click', () => {
     const textArea = document.createElement('textarea')
     const senha = resultadoEl.innerText
- 
+    
+    let senhas = []
+
+    let guardarSenha = () =>{
+        senhas.push({
+            text: textArea.value
+        });
+
+        
+        localStorage.setItem("senhas", JSON.stringify(senhas))
+        console.log(senhas)
+    }
+
     textArea.value = senha 
     document.body.appendChild(textArea)
     textArea.select()
     document.execCommand('copy')
     textArea.remove()
-    alert('Senha copiada!')
+    guardarSenha()
+    /* alert('Senha copiada!') */
+    
   })
+
