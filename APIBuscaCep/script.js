a67588678913f925e35c28eba59e83afd00ae0a9
Adeilsoara <adeilsonaragao@hotmail.com>
2022-10-26 15:40:30 -0300

    APIBuscaCep

---
+++ APIBuscaCep/script.js
@@ -0,0 +1,23 @@
+function retornaValores(conteudo) {
+    if (!("erro" in conteudo)) {
+        document.getElementById('rua').value=(conteudo.logradouro)
+    }else{
+        alert("CEP não encontrado")
+    }
+}
+
+function buscaCep(valor) {
+    var cep = valor.replace(/\D/g, '')
+
+    if (cep != "") {
+        var validarCEP = /^[0-9]{8}$/
+        if (validarCEP.test(cep)) {
+           /*  document.getElementById('rua').value="..." */
+            var script = document.createElement('script')
+            script.src = `https://viacep.com.br/ws/${cep}/json/?callback=retornaValores`
+            document.body.appendChild(script)
+        }else{
+            alert("Formato inválido")
+        }
+    }
+}
