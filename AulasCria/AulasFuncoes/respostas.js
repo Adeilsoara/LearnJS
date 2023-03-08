accdcd77775d95e9dec7519037aca73cd4a7a79b
Adeilson Aragão <adeilsonaragao@hotmail.com>
2023-03-08 18:28:27 -0300

    Update respostas.js

--- AulasCria/AulasFuncoes/respostas.js
+++ AulasCria/AulasFuncoes/respostas.js
@@ -1,3 +1,5 @@
+/********** Lista Turma 01 **********/
+
 
 /* Escreva uma função que retorne o cubo de um número fornecido pelo usuário. */
 
@@ -8,6 +10,19 @@ function cubo() {
 
 document.write(`${num}³ é: `, cubo()) 
 
+/* Criar uma função para calcular a porcentagem de um número informado pelo usuário,
+este deve informar o número e qual a porcentagem que ele deseja saber, por exemplo:
+Usuário informa: 100
+Usuário informa a porcentagem desejada: 10 
+Resultado: 10% de 100 = 10 */
+
+var num = Number(prompt("Informe um número: "))
+var porcentagem = Number(prompt("Qual a porcentagem que deseja saber?: "))
+function calcularPorcentagem(num, porcentagem) {
+    return (num/100)*porcentagem
+}
+
+document.write(`${porcentagem}% de ${num} é: `, calcularPorcentagem(num, porcentagem))
 
 
 /*Criar uma função que converta uma temperatura de Celsius para Fahrenheit os dados de 
