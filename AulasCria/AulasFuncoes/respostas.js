d438cb1e11f84a1241e491f1dcbde6a4bedeac2a
Adeilson Aragão <adeilsonaragao@hotmail.com>
2023-03-08 15:54:05 -0300

    Update respostas.js

--- AulasCria/AulasFuncoes/respostas.js
+++ AulasCria/AulasFuncoes/respostas.js
@@ -1,1 +1,11 @@
+ /*Criar uma função que converta uma temperatura de Celsius para Fahrenheit os dados de 
+ temperatura devem ser informados pelo usuário. Obs.: F = C x 1.8 + 32*/
+
+var celsius = parseFloat(prompt("Informa a temperatura em Celsius: "))
+function celsiusFahrenheit() {
+    return celsius * 1.8 + 32
+}
+
+document.write(`${celsius}º em Fahrenheit é: `, celsiusFahrenheit()) 
+
 
